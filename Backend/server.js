7fd25e2e61a44fb2f4ae5826853d1a722cf3a292
Saksham Chhabra <ccsaksham2006@gmail.com>
2025-09-07 23:04:35 +0530

    CORS Updation server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const FASTAPI_URL = process.env.FASTAPI_URL || 'http://localhost:8000';
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:8080', 'http://localhost:8081', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://127.0.0.1:8081'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:8080', 'http://localhost:8081', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://127.0.0.1:8081', 'https://nova-gig-score.vercel.app'],
   credentials: true
 }));
 
